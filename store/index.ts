b995861b9053d2e7f80c6145bd35ab80177fea48
tavonunez260@gmail.com <tavonunez260@gmail.com>
2023-12-21 22:02:59 -0500

    Undo typing errors.

--- store/index.ts
+++ store/index.ts
@@ -1,10 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 
-import basketReducer from '@/slices/basketSlice';
-
 export * from './StoreProvider';
 export const store = configureStore({
-	reducer: {
-		basket: basketReducer
-	}
+	reducer: {}
 });
