813c2fedeb9387cd0d2fa0180d3f1d4d11665a55
tavonunez260@gmail.com <tavonunez260@gmail.com>
2023-12-21 22:01:33 -0500

    Next config changes.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+	output: 'export',
+	experimental: {
+		appDir: true
+	},
 	images: {
+		unoptimized: true,
 		domains: [
 			'links.papareact.com',
 			'fakestoreapi.com',
