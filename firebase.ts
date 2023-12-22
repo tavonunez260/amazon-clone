309cad1e985339add4a2fb8e02330edd97d40640
tavonunez260@gmail.com <tavonunez260@gmail.com>
2023-12-21 23:29:12 -0500

    Add login.

---
+++ firebase.ts
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const firebaseConfig = {
+	apiKey: 'AIzaSyBW6T30NeHf4nXszlz1m4gasirJBDgQUbA',
+	authDomain: 'project-b89fc.firebaseapp.com',
+	projectId: 'project-b89fc',
+	storageBucket: 'project-b89fc.appspot.com',
+	messagingSenderId: '745376676042',
+	appId: '1:745376676042:web:7e9f463913fabe4326dc33',
+	measurementId: 'G-844ENDZZCW'
+};
