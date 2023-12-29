ece1a739511d2ba049a5c68e85ae65f443cbb0f3
tavonunez260@gmail.com <tavonunez260@gmail.com>
2023-12-29 10:59:06 -0500

    Next auth fix.

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -5,7 +5,14 @@ export const authOptions: NextAuthOptions = {
 	providers: [
 		GoogleProvider({
 			clientId: process.env.GOOGLE_CLIENT_ID as string,
-			clientSecret: process.env.GOOGLE_SECRET as string
+			clientSecret: process.env.GOOGLE_SECRET as string,
+			authorization: {
+				params: {
+					prompt: 'consent',
+					access_type: 'offline',
+					response_type: 'code'
+				}
+			}
 		})
 	],
 	secret: process.env.NEXTAUTH_SECRET as string
