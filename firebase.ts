3fd12e8881e5c37b85a7eba21695feca03a25460
Gustavo Nunez Ortiz <tavonunez260@gmail.com>
2024-01-15 10:30:56 -0500

    Firebase config file.

---
+++ firebase.ts
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+	apiKey: process.env.API_KEY,
+	authDomain: process.env.AUTH_DOMAIN,
+	projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+	storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+	messagingSenderId: process.env.MESSAGING_SENDER_ID,
+	appId: process.env.APP_ID,
+	measurementId: process.env.MEASUREMENT_ID
+};
